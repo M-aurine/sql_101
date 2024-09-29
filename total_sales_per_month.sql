38213b083721d6d33f12f399ad1dc359d38c093d
Maurine Nyongesa <nmaurine342@gmail.com>
2024-09-29 12:22:47 +0300

    Add all SQL files for Sales Analysis

---
+++ total_sales_per_month.sql
@@ -0,0 +1,7 @@
+SELECT 
+  EXTRACT(YEAR FROM SaleDate) AS SaleYear, 
+  EXTRACT(MONTH FROM SaleDate) AS SaleMonth, 
+  SUM(Amount) AS TotalSales
+FROM Sales
+GROUP BY SaleYear, SaleMonth
+ORDER BY SaleYear, SaleMonth;
